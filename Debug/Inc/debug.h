4271612bb0b0a3207cc80c9c15c764f46f30c68b
Tsvetomir Iliev <tsi.web.design@gmail.com>
2022-05-27 13:27:53 +0300

    initial commit

---
+++ Debug/Inc/debug.h
@@ -0,0 +1,17 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+void DBG_printi(char * message,...);
+
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+#define DBG_ENABLE_PRINTF
+#define DBG_ENABLE_PRINTI
+
+#define DBG_TIMEOUT         1000
+
+#endif
---
+++ Debug/debug.c
@@ -0,0 +1,31 @@
+#include "debug.h"
+
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "main.h"
+
+static char dtx_buffer[64];
+extern UART_HandleTypeDef huart3;
+
+#ifdef DBG_ENABLE_PRINTF
+PUTCHAR_PROTOTYPE
+{
+  HAL_UART_Transmit(&huart3, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  return ch;
+}
+#endif
+
+void DBG_printi(char * message,...) {
+    #ifdef DBG_ENABLE_PRINTI
+        va_list args;
+
+        va_start(args, message);
+        vsprintf(dtx_buffer, message, args);
+        va_end (args);
+
+        //transmit the message over UART3
+        HAL_UART_Transmit(&huart3,(uint8_t*)dtx_buffer, strlen(dtx_buffer), DBG_TIMEOUT);
+    #endif
+}
